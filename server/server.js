718c7d8c25f89fa0339950236dff8bbf7e101f21
Jason Marshall <jasonnz@gmail.com>
2017-03-04 22:15:38 +1300

    Socket connection and disconnect event emitters

--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io')
+const port = process.env.PORT || 3000;
 
 console.log(__dirname + '/../public');
 console.log(publicPath);
 
 var app = express();
-const port = process.env.PORT || 3000;
-
-// app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//    res.send(200);
-// });
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket)=> {
+    console.log('New user connected');
+
+    socket.on('disconnect', (socket)=> {
+        console.log('Disconnected from client');
+    });
+});
+
+server.listen(port, ()=> {
     console.log('Started at port ', port);
 });
