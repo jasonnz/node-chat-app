626c2f5bdffecd25301cdcbe2e527858239f9f92
Jason Marshall <jasonnz@gmail.com>
2017-03-14 21:57:34 +1300

    Added moment.js functionality to app

--- public/js/index.js
+++ public/js/index.js
@@ -11,18 +11,20 @@ socket.on('disconnect', function() {
 // custom
 socket.on('newMessage', function(message) {
     console.log('Received a new message from the server: ', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
     console.log('Received a new location from the server: ', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a  = $('<a target="_blank">My current location</a>');
     
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
