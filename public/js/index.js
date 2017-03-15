42e099a7061cb748c1614820f8a53bf30c0d4acd
Jason Marshall <jasonnz@gmail.com>
2017-03-15 22:04:46 +1300

    Added Mustache.js templates

--- public/js/index.js
+++ public/js/index.js
@@ -12,22 +12,28 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(message) {
     console.log('Received a new message from the server: ', message);
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
     console.log('Received a new location from the server: ', message);
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a  = $('<a target="_blank">My current location</a>');
+    var template = $('#location-message-template').html();
     
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
 });
 
 socket.emit('createMessage', {
