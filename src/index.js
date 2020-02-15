f109f192708617956291b20a6c1afb819212c275
Jason Marshall <jasonnz@gmail.com>
2020-02-15 17:30:19 +1300

    Client side Javascript

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
 const port = process.env.PORT || 3000
-
+const io = socketio(server)
 
 // Setup static directory to server
-app.use(express.static(path.join(__dirname, '../public/html')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 // App get for route handling
 app.get('/index', (req, res) => {
     res.render('index')
 })
 
+io.on('connection', () => {
+    console.log('New web socket connection')
+})
+
 // Listen on port
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server started on port ', port)
 })
